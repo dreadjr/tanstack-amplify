58f7d6760ff71e984fdbd75ca04b4600ec576f72
doug <dreadjr@gmail.com>
2025-04-09 17:19:11 -0700

    git commit -m "first commit"

---
+++ app.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@tanstack/react-start/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  tsr: {
+    appDirectory: 'src',
+  },
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+    ],
+  },
+  server: {
+    preset: 'aws-amplify',
+  },
+  // https://nitro.build/deploy/providers/aws-amplify
+
+})
